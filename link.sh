8b1b6f83591d15d218d7fe34144b8012508a6e98
Ethan Lynn <ethan@sabado.com>
2022-02-26 22:12:01 -0500

    feat: add .gitignore

--- link.sh
+++ link.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
 
-ln -sf $(pwd)/.zshrc $HOME/.zshrc
+ln -sf $(pwd)/.gitignore $HOME/.gitignore
 ln -sf $(pwd)/.vscode/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
+ln -sf $(pwd)/.zshrc $HOME/.zshrc
