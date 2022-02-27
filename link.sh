93305df052b1321c49304f55a2e8c4309287469b
Ethan Lynn <ethan@sabado.com>
2022-02-27 10:06:31 -0500

    feat: add .gitconfig

--- link.sh
+++ link.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
+ln -sf $(pwd)/.gitconfig $HOME/.gitconfig
 ln -sf $(pwd)/.gitignore $HOME/.gitignore
 ln -sf $(pwd)/.vscode/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
 ln -sf $(pwd)/.zshrc $HOME/.zshrc
